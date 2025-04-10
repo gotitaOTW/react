7ce3c14e230a89a9b4ad564978a61dbca63cd685
THIAGO MARTIN GARCIA SALMASO <48112397@ort>
2025-04-10 12:26:57 -0300

    first

---
+++ primerProyectoReact/src/assets/componentes.jsx
@@ -0,0 +1,15 @@
+function mostrarFormulario({titulo}, {nombre}, {fechaNac}, {descripcion}, {valorBoton}){
+    return (
+        <>
+        <p>hola</p>
+        </>
+    )
+    }
+//mostrarCita({})
+
+function saludar({nombre}){
+return(
+    <div>
+        <p>hola {nombre}</p>
+    </div>
+)}
