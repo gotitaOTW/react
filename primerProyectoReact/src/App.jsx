802e0fe621ce9e82eeda3d1e39c9b03509cdcbb8
THIAGO MARTIN GARCIA SALMASO <48112397@ort>
2025-04-16 10:40:35 -0300

    a

--- primerProyectoReact/src/App.jsx
+++ primerProyectoReact/src/App.jsx
@@ -3,13 +3,34 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import './index.css'
+import './ajaxlibs.css'
 import MostrarForm from './assets/componentes.jsx'
+import MostrarCitas from './assets/mostrarCitas.jsx'
+import MostrarCita from './assets/citas.jsx'
 
 function App() {
   return (
     <>
-      <h1>ADMINISTRADOR DE PACIENTES</h1>
-      <MostrarForm titulo="Inscripcion" inputText1="Nombre" inputText2="Apellido" inputText3="Descripción" submit="enviar datos"/>
+           <h1>ADMINISTRADOR DE PACIENTES</h1>
+
+    <div id="root">
+      <div className="container">
+        <div className="row">
+          <div className="one-half column">
+            <MostrarForm titulo="Inscripcion" inputText1="Nombre" inputText2="Apellido" inputText3="Descripción" submit="Inscribirse"/> 
+           </div>
+          <div className="one-half column">
+            <h2>Citas</h2>
+            <MostrarCita num="2"/>  
+            <MostrarCita num="1"/>
+            <MostrarCita num="3"/>
+          </div>
+        </div>
+      </div>
+    </div>
+    <script src="/static/js/bundle.js"></script>
+    <script src="/static/js/vendors~main.chunk.js"></script>
+    <script src="/static/js/main.chunk.js"></script>
     </>
   )
 }
--- primerProyectoReact/src/assets/componentes.jsx
+++ primerProyectoReact/src/assets/componentes.jsx
@@ -9,36 +9,36 @@ function MostrarForm({ titulo, inputText1, inputText2, inputText3, submit}) {
                 <input
                     type="text"
                     name="mascota"
-                    class="u-full-width"
-                    placeholder={input1}
+                    className="u-full-width"
+                    placeholder={inputText1}
                 />
                 <label>{inputText2}</label>
                 <input
                     type="text"
                     name="propietario"
-                    class="u-full-width"
+                    className="u-full-width"
                     placeholder={inputText2}
                 />
                 <label>Fecha</label>
                 <input
                     type="date"
                     name="fecha"
-                    class="u-full-width"
+                    className="u-full-width"
                 />
                 <label>Hora</label>
                 <input
                     type="time"
                     name="hora"
-                    class="u-full-width"
+                    className="u-full-width"
                 />
                 <label>{inputText3}</label>
                 <textarea
                     name="sintomas"
-                    class="u-full-width"
+                    className="u-full-width"
                 ></textarea>
                 <button
                     type="submit"
-                    class="u-full-width button-primary"
+                    className="u-full-width button-primary"
                 >
                     {submit}
                 </button>
@@ -46,5 +46,7 @@ function MostrarForm({ titulo, inputText1, inputText2, inputText3, submit}) {
         </div>
     );
 }
+
+
 
 export default MostrarForm;
