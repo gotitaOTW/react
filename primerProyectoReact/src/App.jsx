b6a9a8702504b8ea7337f85e9690ce193c441f19
THIAGO MARTIN GARCIA SALMASO <48112397@ort>
2025-04-16 09:28:13 -0300

    a

--- primerProyectoReact/src/App.jsx
+++ primerProyectoReact/src/App.jsx
@@ -2,14 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import saludar from './assets/componentes.jsx'
+import './index.css'
+import MostrarForm from './assets/componentes.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-    <saludar nombre="thiaguito"/>
+      <h1>ADMINISTRADOR DE PACIENTES</h1>
+      <MostrarForm titulo="Inscripcion" inputText1="Nombre" inputText2="Apellido" inputText3="Descripción" submit="enviar datos"/>
     </>
   )
 }
--- primerProyectoReact/src/assets/componentes.jsx
+++ primerProyectoReact/src/assets/componentes.jsx
@@ -1,15 +1,50 @@
-function mostrarFormulario({titulo}, {nombre}, {fechaNac}, {descripcion}, {valorBoton}){
-    return (
-        <>
-        <p>hola</p>
-        </>
-    )
-    }
-//mostrarCita({})
+import React from "react";
 
-function saludar({nombre}){
-return(
-    <div>
-        <p>hola {nombre}</p>
-    </div>
-)}
+function MostrarForm({ titulo, inputText1, inputText2, inputText3, submit}) {
+    return (
+        <div>
+            <h2>{titulo}</h2>
+            <form>
+                <label>{inputText1}</label>
+                <input
+                    type="text"
+                    name="mascota"
+                    class="u-full-width"
+                    placeholder={input1}
+                />
+                <label>{inputText2}</label>
+                <input
+                    type="text"
+                    name="propietario"
+                    class="u-full-width"
+                    placeholder={inputText2}
+                />
+                <label>Fecha</label>
+                <input
+                    type="date"
+                    name="fecha"
+                    class="u-full-width"
+                />
+                <label>Hora</label>
+                <input
+                    type="time"
+                    name="hora"
+                    class="u-full-width"
+                />
+                <label>{inputText3}</label>
+                <textarea
+                    name="sintomas"
+                    class="u-full-width"
+                ></textarea>
+                <button
+                    type="submit"
+                    class="u-full-width button-primary"
+                >
+                    {submit}
+                </button>
+            </form>
+        </div>
+    );
+}
+
+export default MostrarForm;
